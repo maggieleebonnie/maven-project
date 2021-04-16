9750027058989c1b631634b4d60f39f56a56c7d7
maggieleebonnie <82593792+maggieleebonnie@users.noreply.github.com>
2021-04-16 15:10:15 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,10 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/**
+* @param someone name of a person
+* @return greeging string
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
